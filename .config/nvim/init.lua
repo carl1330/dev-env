f4fc01a9581a1ee9793fa096bd3b95fc1a55585e
Carl Gulliksson <carl.gulliksson@agilycom.com>
2025-05-09 18:04:34 +0200

    add more stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,19 @@
 require("carlgulliksson")
 require("config.lazy")
 
-vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.wo.number = true
+
+vim.filetype.add({
+  extension = {
+    jinja2 = "jinja",
+  },
+})
+
+
--- .config/nvim/lua/carlgulliksson/remap.lua
+++ .config/nvim/lua/carlgulliksson/remap.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>r", vim.cmd.edit)
+vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true }) -- Copy to system clipboard in visual mode
+vim.keymap.set("n", "<leader>Y", '"+Y', { noremap = true, silent = true }) -- Copy whole line to system clipboard
+
 vim.keymap.set("n", "<leader>x", function() 
     vim.cmd("!chmod +x %")
 end, { silent = true })
