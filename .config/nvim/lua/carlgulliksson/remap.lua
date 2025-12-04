8134b505080fcd44394e0f3e50685291321529c4
Carl Gulliksson <carl.gulliksson@agilycom.com>
2025-12-04 13:55:51 +0100

    fix: add open error box

--- .config/nvim/lua/carlgulliksson/remap.lua
+++ .config/nvim/lua/carlgulliksson/remap.lua
@@ -4,8 +4,9 @@ vim.keymap.set("n", "<leader>r", vim.cmd.edit)
 vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true }) -- Copy to system clipboard in visual mode
 vim.keymap.set("n", "<leader>Y", '"+Y', { noremap = true, silent = true }) -- Copy whole line to system clipboard
 
-vim.keymap.set("n", "<leader>x", function() 
-    vim.cmd("!chmod +x %")
+vim.keymap.set("n", "<leader>x", function()
+	vim.cmd("!chmod +x %")
 end, { silent = true })
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>")
