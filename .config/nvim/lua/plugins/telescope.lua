42bc5e30bb440aaa1ff33b24419d586538a00e64
carl1330 <carl.gulliksson@compileit.com>
2026-01-13 15:50:28 +0100

    fix: telescope issue with treesitter

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,20 +1,27 @@
 return {
-     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-     dependencies = { 
-	     'nvim-lua/plenary.nvim',
-	     'BurntSushi/ripgrep'
-     },
-     config = function()
-	local builtin = require('telescope.builtin')
-	vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-	vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-	vim.keymap.set('n', '<leader>ps', function()
-		builtin.grep_string({ search = vim.fn.input("Grep > ")})
-	end)
-	vim.keymap.set('n', '<leader>pF', function()
-		builtin.live_grep({search_dirs={"."}})
-	end)
-     end
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"BurntSushi/ripgrep",
+	},
+	config = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>ps", function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end)
+		vim.keymap.set("n", "<leader>pF", function()
+			builtin.live_grep({ search_dirs = { "." } })
+		end)
+
+		require("telescope").setup({
+			defaults = {
+				preview = {
+					treesitter = false,
+				},
+			},
+		})
+	end,
 }
-
-
